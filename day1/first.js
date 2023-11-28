3e98f61a36eb92fbaca809ffb4d7ffb3b22caddf
Jatin Bansal <home.minj@gmail.com>
2023-11-28 18:16:26 +0545

    tried pushing

---
+++ day1/first.js
@@ -0,0 +1,11 @@
+const profile=
+{
+userName: "shardakhapra",
+posts:195,
+followers:"569k",
+following:4,
+bio:"Enterpreneur",
+threadId:"sharda khapra",
+};
+console.log(profile);
+console.log(typeof ["userName"]);
