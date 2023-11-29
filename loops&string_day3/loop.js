1ad1845a896ea54b4f563f3e2532ababb29e9022
Jatin Bansal <home.minj@gmail.com>
2023-11-29 23:57:24 +0545

    10% of loop done

---
+++ loops&string_day3/loop.js
@@ -0,0 +1,17 @@
+// console.log("hello man")
+// for(let i=1;i<=5;i++){
+//     console.log("humans are great");
+// }
+
+// // console.log("loops has ended")
+// let sum=0;
+// for(i=1;i<=5;i++){
+//     sum=i+sum;
+// }
+// console.log("The sum is",sum);
+
+let sub=0;
+for(i=1;i=>10;i--){
+    console.log(sub=i-sub);
+}
+console.log("sub is",sub)
